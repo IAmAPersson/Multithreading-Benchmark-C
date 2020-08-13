18a71226e41200832f20f02e3d543174e4dfe668
iamaperson620 <iamaperson620@gmail.com>
2020-08-13 00:54:49 -0500

    ignoring unused result warnings pog

--- main.c
+++ main.c
@@ -3,13 +3,28 @@
 #include <ctype.h>
 #include <stdbool.h>
 #include <omp.h>
+#include <string.h>
 
 int main(int argc, char** argv)
 {
 	unsigned int threads = 0;
-	bool FPU;
+	bool FPU, detailed = false;
 	char FPU_char;
 	int score = 0;
+	
+	//See if they want a detailed score
+	if (argc == 2 && !strcmp(argv[1], "-d"))
+		detailed = true;
+	
+	//See if they help
+	if (argc == 2 && !strcmp(argv[1], "-h"))
+	{
+		printf("There is only one flag, and it is -d, which outputs a more detailed score. The rest is self-explanatory.\n");
+		return 0;
+	}
+
+	#pragma GCC diagnostic push
+	#pragma GCC diagnostic ignored "-Wunused-result"
 
 	//Get what you want to stress
 	printf("Would you like to stress floating-point (F) or integer (I)?\n");
@@ -34,13 +49,15 @@ int main(int argc, char** argv)
 
 	//Get number of threads
 	printf("Enter thread count to run test, or enter 0 to automatically run with the number of threads detected on your CPU:\n");
-	scanf("%d", &threads);
+	scanf("%ud", &threads);
+	
+	#pragma GCC diagnostic pop
 
 	//Run the benchmark
 	if (threads == 0)
-		score = benchmark(FPU);
+		score = benchmark(FPU, detailed);
 	else
-		score = benchmarkThreads(threads, FPU);
+		score = benchmarkThreads(threads, FPU, detailed);
 
 	//Display the score
 	printf("Score: %d\n", score);
